c73d332804114e84eeb7902c202977715b1d9894
todor-grigorov <t.grigorov99@gmail.com>
2023-06-25 12:33:03 +0200

    feat: update Category model with collection of Properties

--- SimpleRealEstateApi/Models/Category.cs
+++ SimpleRealEstateApi/Models/Category.cs
@@ -9,5 +9,6 @@ namespace SimpleRealEstateApi.Models
         public string Name { get; set; }
         [Required(ErrorMessage = "Image url can't be null or empty")]
         public string ImageUrl { get; set; }
+        public ICollection<Property> Properties { get; set; }
     }
 }
