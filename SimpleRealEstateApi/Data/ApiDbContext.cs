fda41c0b7e9a998b42c3d5b99496d5d186e53eff
todor-grigorov <t.grigorov99@gmail.com>
2023-06-25 12:44:38 +0200

    feat: add DbContext class with connection string

---
+++ SimpleRealEstateApi/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleRealEstateApi.Models;
+
+namespace SimpleRealEstateApi.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=RealEstateDb");
+        }
+    }
+}
