c1b9f97b746c2e89b0f25fc6123e5f558ac6b814
todor-grigorov <t.grigorov99@gmail.com>
2023-06-27 23:31:18 +0200

    refactor: userDtop

--- SimpleRealEstateApi/Dto/UserDto.cs
+++ SimpleRealEstateApi/Dto/UserDto.cs
@@ -5,10 +5,11 @@ namespace SimpleRealEstateApi.Dto
 {
     public class UserDto
     {
-        public int Id { get; set; }
+        //public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public ICollection<Property> Properties { get; set; }
+        public string Password { get; set; }
+        //public ICollection<Property> Properties { get; set; }
     }
 }
