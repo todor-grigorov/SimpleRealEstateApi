ce5fb064c89316b686d5a08e28570fa5cde13677
todor-grigorov <t.grigorov99@gmail.com>
2023-06-27 22:06:15 +0200

    feat: add UserRepository

---
+++ SimpleRealEstateApi/Repository/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using SimpleRealEstateApi.Data;
+using SimpleRealEstateApi.Interfaces;
+using SimpleRealEstateApi.Models;
+
+namespace SimpleRealEstateApi.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApiDbContext _context;
+
+        public UserRepository(ApiDbContext context) {
+            _context = context;
+        }
+        public bool Register(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            var saved = _context.SaveChanges();
+            return saved > 0 ? true : false;
+        }
+    }
+}
