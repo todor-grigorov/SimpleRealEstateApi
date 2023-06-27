55132964e2142bce8fe6c0cfa9bbfb771784209b
todor-grigorov <t.grigorov99@gmail.com>
2023-06-27 21:53:10 +0200

    feat: add Users Controller

---
+++ SimpleRealEstateApi/Controllers/UsersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SimpleRealEstateApi.Data;
+using SimpleRealEstateApi.Models;
+
+namespace SimpleRealEstateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        ApiDbContext _dbContext = new ApiDbContext();
+
+        [HttpPost]
+        public IActionResult Register([FromBody] User user)
+        {
+
+        }
+    }
+}
