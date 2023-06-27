e3c3d5e0a0ee79a783ef1242d619c80a4164609f
todor-grigorov <t.grigorov99@gmail.com>
2023-06-27 22:01:55 +0200

    feat: add IUserRepository interface

---
+++ SimpleRealEstateApi/Interfaces/IUserRepository.cs
@@ -0,0 +1,10 @@
+﻿using SimpleRealEstateApi.Models;
+
+namespace SimpleRealEstateApi.Interfaces
+{
+    public interface IUserRepository
+    {
+        bool Register(User user);
+        bool Save();
+    }
+}
