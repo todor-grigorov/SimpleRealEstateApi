e3149aa227d09df9ffd05c6220f019e6656ec61c
todor-grigorov <t.grigorov99@gmail.com>
2023-06-27 22:38:42 +0200

    feat: add UserDto

---
+++ SimpleRealEstateApi/Dto/UserDto.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace SimpleRealEstateApi.Dto
+{
+    public class UserDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public ICollection<Property> Properties { get; set; }
+    }
+}
