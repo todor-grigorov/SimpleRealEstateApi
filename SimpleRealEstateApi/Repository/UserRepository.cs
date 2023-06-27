8f03508ef78b768c7a062547c7634bf04541ebf3
todor-grigorov <t.grigorov99@gmail.com>
2023-06-27 22:53:35 +0200

    feat: implement UserExistsById

--- SimpleRealEstateApi/Repository/UserRepository.cs
+++ SimpleRealEstateApi/Repository/UserRepository.cs
@@ -11,15 +11,27 @@ namespace SimpleRealEstateApi.Repository
         public UserRepository(ApiDbContext context) {
             _context = context;
         }
-        public bool Register(User user)
+
+        public bool CreateUser(User user)
         {
             throw new NotImplementedException();
         }
+
+        public User GetUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
 
         public bool Save()
         {
             var saved = _context.SaveChanges();
             return saved > 0 ? true : false;
+        }
+
+        public bool UserExistsById(int id)
+        {
+            return _context.Users.Any(u => u.Id == id);
         }
     }
 }
