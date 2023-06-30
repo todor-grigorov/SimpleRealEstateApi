40113fce86e9444eb3c5cac95944d23403b80f64
todor-grigorov <t.grigorov99@gmail.com>
2023-06-30 21:14:58 +0200

    feat: add Category Controller

---
+++ SimpleRealEstateApi/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SimpleRealEstateApi.Dto;
+using SimpleRealEstateApi.Interfaces;
+using SimpleRealEstateApi.Models;
+
+namespace SimpleRealEstateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryRepository _categoryRepository;
+        private readonly IMapper _mapper;
+
+        public CategoryController(ICategoryRepository categoryRepository, IMapper mapper)
+        {
+            _categoryRepository = categoryRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Category>))]
+        public IActionResult GetCategories()
+        {
+            var categories = this._mapper.Map<List<CategoryDto>>(this._categoryRepository.GetCategories());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok(categories);
+        }
+    }
+}
