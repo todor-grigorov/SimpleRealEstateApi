a825030c500625bdbdd4319125bdf7b30c19f241
todor-grigorov <t.grigorov99@gmail.com>
2023-06-30 21:11:34 +0200

    feat: implement Category Repository

--- SimpleRealEstateApi/Repository/CategoryRepository.cs
+++ SimpleRealEstateApi/Repository/CategoryRepository.cs
@@ -1,6 +1,21 @@
-﻿namespace SimpleRealEstateApi.Repository
+﻿using SimpleRealEstateApi.Data;
+using SimpleRealEstateApi.Interfaces;
+using SimpleRealEstateApi.Models;
+
+namespace SimpleRealEstateApi.Repository
 {
-    public class CategoryRepository
+    public class CategoryRepository : ICategoryRepository
     {
+        private readonly ApiDbContext _context;
+
+        public CategoryRepository(ApiDbContext context)
+        {
+            _context = context;
+        }
+
+        public ICollection<Category> GetCategories()
+        {
+            return _context.Categories.ToList();
+        }
     }
 }
