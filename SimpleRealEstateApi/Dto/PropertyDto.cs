6acbc9783a081179e9f848b47f300d7090bce52c
todor-grigorov <t.grigorov99@gmail.com>
2023-07-02 11:38:46 +0200

    feat: add Proeprty Dto

---
+++ SimpleRealEstateApi/Dto/PropertyDto.cs
@@ -0,0 +1,17 @@
+﻿using SimpleRealEstateApi.Models;
+
+namespace SimpleRealEstateApi.Dto
+{
+    public class PropertyDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Detail { get; set; }
+        public string Address { get; set; }
+        public string ImageUrl { get; set; }
+        public double Price { get; set; }
+        public bool IsTrending { get; set; }
+        public int CategoryId { get; set; }
+        public int UserId { get; set; }
+    }
+}
