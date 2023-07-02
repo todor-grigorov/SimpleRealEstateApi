907022ea2a42f0364de863569096d2a964d70e9e
todor-grigorov <t.grigorov99@gmail.com>
2023-07-02 12:12:23 +0200

    feat: register Properties repository

--- SimpleRealEstateApi/Program.cs
+++ SimpleRealEstateApi/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddControllers();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPropertiesRepository, PropertiesRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
