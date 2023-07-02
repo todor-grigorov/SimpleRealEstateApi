afe22369d32f99d90a546ccc79b5caca5fd30777
todor-grigorov <t.grigorov99@gmail.com>
2023-07-02 11:44:12 +0200

    feat: init PropertiesController

---
+++ SimpleRealEstateApi/Controllers/PropertiesController.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SimpleRealEstateApi.Interfaces;
+
+namespace SimpleRealEstateApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PropertiesController : Controller
+    {
+        private readonly IPropertiesRepository _propertiesRepository;
+        private readonly IMapper _mapper;
+
+        public PropertiesController(IPropertiesRepository propertiesRepository, IMapper mapper)
+        {
+            _propertiesRepository = propertiesRepository;
+            _mapper = mapper;
+        }
+    }
+}
